a9c5a3ed15c5b135e8cd5ba79e7e06989737d70c
jemima Anyanah <northernweavegh@gmail.com>
2024-10-08 13:37:31 +0000

    worked on form

--- src/pages/addBook/index.jsx
+++ src/pages/addBook/index.jsx
@@ -2,14 +2,29 @@ import Footer from "../../components/Footer";
 import Navbar from "../../components/Navbar";
 
 
-const AddBook = () => {
+const AddBooks = () => {
   return (
     <>
       <Navbar />
+      <div className="flex justify-center items-center flex-col h-screen bg-gray-100 space-x-3">
+        <h1>Add A New Book</h1>
+        <form className="flex flex-col">
+          <div>
+            <input type="text" placeholder="Enter Book Title" reguired name="Book" />
+          </div>
+          <div>
+            <input type="text" placeholder="Enter Author" required name="Book" />
+          </div>
+          <div>
+            <input type="text" placeholder="Enter Genre" required name="Book" />
+          </div>
+         <button type="submit">Add Book</button>
+        </form> 
+      </div>
       <Footer />
     </>
 
   )
 }
 
-export default AddBook;
+export default AddBooks;
