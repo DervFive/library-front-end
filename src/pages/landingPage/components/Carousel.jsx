4e3b51c13d25b8681bb1a20c474ca927403a395e
Samuel Bright Bakah <samuelbrightsk@gmail.com>
2024-10-09 11:37:05 +0000

    worked on landing and added more JS

--- src/pages/landingPage/components/Carousel.jsx
+++ src/pages/landingPage/components/Carousel.jsx
@@ -1,28 +1,57 @@
 
+import LandingText from "./LandingText";
+
 
 const Carousel = () => {
     return (
-        <div className="carousel h-[calc(100vh-70px)] mt-[-50px] overflow-hidden relative ">
+        <div className="carousel h-[calc(100vh-70px)] m-[30px] rounded-[20px] overflow-hidden relative flex items-center justify-center bg-gray-100">
 
             <div className="list">
 
-                <div className="landing-page w-[180px] h-[250px] rounded-[20px] bg-[url('./src/assets/images/harry-porter.jpg')] " >
+
+                <div className="landing-page w-[180px] h-[250px] rounded-[20px] bg-[url('./src/assets/images/otegha.jpg')] " >
 
                 </div>
-                <div className="landing-page w-[180px] h-[250px] rounded-[20px]  ">
+
+                <div className="landing-page w-[180px] h-[250px] rounded-[20px] bg-[url('./src/assets/images/collection.jpg')] ">
+                    {/* <LandingText /> */}
 
                 </div>
-                <div className="landing-page w-[180px] h-[250px] rounded-[20px] bg-[url('./src/assets/images/janne.jpg') ">
+
+                <div className="landing-page w-[180px] h-[250px] rounded-[20px] bg-[url('./src/assets/images/janne.jpg')] ">
 
                 </div>
-                <div className="landing-page w-[180px] h-[250px] rounded-[20px] ">
+
+                <div className="landing-page w-[180px] h-[250px] rounded-[20px] bg-[url('./src/assets/images/ikigai.jpg')]">
 
                 </div>
-                <div className="landing-page w-[180px] h-[250px] rounded-[20px] ">
+
+                <div className="landing-page w-[180px] h-[250px] rounded-[20px] bg-[url('./src/assets/images/harry-porter.jpg')] ">
+
+
+                </div>
+
+                <div className="landing-page w-[180px] h-[250px] rounded-[20px] bg-[url('./src/assets/images/nikita-porter.jpg')] ">
+
+                </div>
+
+                <div className="landing-page w-[180px] h-[250px] rounded-[20px] bg-[url('./src/assets/images/harry-porter.jpg')] ">
+
+                </div>
+
+                <div className="landing-page w-[180px] h-[250px] rounded-[20px] bg-[url('./src/assets/images/harry-porter.jpg')] ">
 
                 </div>
 
             </div>
+
+            <div className="arrows flex gap-[10px] items-center text-white text-[10px] ">
+                <button className="prev"><div><i class="fa-solid fa-chevron-left"></i></div></button>
+                <button className="next"><div><i class="fa-solid fa-chevron-right"></i></div></button>
+
+            </div>
+
+            <div className="timeRunning"></div>
 
             {/* <div className=" flex justify-end bg-red-600/20 h-full rounded-[20px]">
 
